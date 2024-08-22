3f58b79f2c846485ef37a0d9cc503d6b06e7d966
Zhe1224 <146051385+Zhe1224@users.noreply.github.com>
2024-08-22 19:49:36 +0800

    Create NetBeans Project

---
+++ src/main/java/com/tarumtrsw2407/donationapp202407/DonationApp202407.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tarumtrsw2407.donationapp202407;
+
+/**
+ *
+ * @author vk_wong
+ */
+public class DonationApp202407 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
