6ae6f59ac8fe6c6215cb874427fef811503964f4
Zhe1224 <146051385+Zhe1224@users.noreply.github.com>
2024-08-30 12:35:07 +0800

    Create Menu.java

---
+++ src/main/java/com/tarumtrsw2407/donationapp202407/Menu.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public final class Menu{
+  public static int prompt(String question,String[] choices){
+    //for
+    /*
+    {
+      
+    }
+    System.out.println(question);
+    String input=new Scanner(System.in).nextLine();
+    */
+    //for
+    
+    return -1;
+  }
+}
