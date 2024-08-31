41bcf3440c7f3ab86bd376cc784c6e53c06e7acd
Zhe1224 <wellwhat8@gmail.com>
2024-08-31 13:54:20 +0800

    ADT interface (subject to change)

--- src/main/java/com/tarumtrsw2407/donationapp202407/adt/ArrayList.java
+++ src/main/java/com/tarumtrsw2407/donationapp202407/adt/ArrayList.java
@@ -2,68 +2,150 @@ package com.tarumtrsw2407.donationapp202407.adt;
 
 import java.io.Serializable;
 import static java.util.Arrays.copyOfRange;
+import java.util.Iterator;
 
 /**
  * ArrayList.java A class that implements the ADT List using an array.
  *
- * @author Frank M. Carrano
- * @version 2.0
- * @param <T>
+ * @author Zhe1224
+ * @version ???
+ * @param <T> type of items
  */
-public class ArrayList<T> implements ListInterface<T>, Serializable {
+public final class ArrayList<T> implements ListInterface<T>, Serializable{
+    private class ListIterator implements Iterator<T>{
+        private int nextIndex=0;
+        @Override
+        public boolean hasNext(){
+            return nextIndex<size();
+        }
+        @Override
+        public T next(){
+            if (!hasNext()) return null;
+            return at(nextIndex++);
+        }
+    }
     private T[] array;
-    private int numberOfEntries;
+    private int size;
     private static final int DEFAULT_CAPACITY = 16;
+    /*Generic constructors*/
     public ArrayList() {
         this(DEFAULT_CAPACITY);
     }
     public ArrayList(int initialCapacity) {
-        numberOfEntries = 0;
-        array = (T[]) new Object[initialCapacity];
+        size = 0;
+        array = (T[]) new Object[initialCapacity>0?initialCapacity:DEFAULT_CAPACITY];
     }
+    public ArrayList(ListInterface<T> items){
+        this(items.size());
+        for (T item:items){this.append(item);}
+    }
+    /*Special constructors*/
+    public ArrayList(T[] items){
+        this(items.length);
+        this.append(items);
+    }
+    public ArrayList(ArrayList<T> items){
+        this(items.getItemsAsArray(0,items.size()));
+    }
+    /*Generic getters*/
+    @Override
+    public int size() {
+        return size;
+    }
+    @Override
     public T at(int pos){
         return array[pos];
     }
-    public T[] getItems(int start, int end) {
-        return copyOfRange(array, start, end);
+    @Override
+    public ArrayList<T> getItems(int start, int end){
+        return new ArrayList<>(getItemsAsArray(start, end));
     }
-    public ArrayList<Integer> getPosOf(T item){
-        return getPosOf(wrap(item))[0];
-    }
-    public ArrayList<Integer>[] getPosOf(T[] items){
-        ArrayList<Integer>[] output=(ArrayList<Integer>[]) new Object[items.length];
-        int i=0;
-        for (T element:array){
-            int j=0;
-            for (T item:items) {
-                if (element.equals(item)) output[j].append(i);
-                j++;
-            }
-            i++;
-        }
+    @Override
+    public ArrayList<Integer> getPosOf(T item,boolean global){
+        ArrayList<Integer> output=new ArrayList<>();
+        for (int i=0;i<size();i++) if (this.at(i).equals(item)) {output.append(i);if (!global) return output;}
         return output;
     }
-    private T[] wrap(T item){
-        T[] items=(T[]) new Object[1];items[0]=item;
-        return items;
+    @Override
+    public ArrayList<ListInterface<Integer>> getPosOf(ListInterface<T> items,boolean global){
+        ArrayList<ListInterface<Integer>>output=new ArrayList<>();
+        for (T item:items){output.append(getPosOf(item,global));}
+        return output;
     }
-    public boolean append(T item){
+    @Override
+    public Iterator<T> getIterator() {
+        return new ListIterator();
+    }
+    /*Special getters*/
+    public T[] getItemsAsArray(int start, int end){
+        return copyOfRange(array, start, end);
+    }
+    public ArrayList<ListInterface<Integer>> getPosOf(T[] items){
+        ArrayList<ListInterface<Integer>>output=new ArrayList<>();
+        for (T element;array) 
+        for (int i=0;i<size();)
+        for (T item:items){output.append(getPosOf(item));}
+        return output;
+    }
+    public ArrayList<ListInterface<Integer>> getPosOf(ArrayList<T> items){
+        return getPosOf(items.getItemsAsArray(0,items.size()));
+    }
+    /*Generic mutators(create)*/
+    @Override
+    public int append(T item){
         return append(wrap(item));
     }
-    public boolean append(T[] items){
-        return insert(numberOfEntries,items);
+    @Override
+    public int append(ListInterface<T> items){
+        for (T item:items){append(item);}
+        return size();
     }
-    public boolean insert(int pos,T item){
+    @Override
+    public int insert(int pos,T item){
         return insert(pos,wrap(item));
     }
-    public boolean insert(int pos,T[] items){
-        splice(pos,0,items);
-        return true;
+    @Override
+    public int insert(int pos,ListInterface<T> items){
+        return insert(new ArrayList<T>(items));
     }
+    /*Special mutators(create)*/
+    public int append(T[] items){
+        return insert(size(),items);
+    }
+    public int append(ArrayList<T> items){
+        return append(items.getItemsAsArray(0, items.size()));
+    }
+    public int insert(int pos,T[] items){
+        splice(pos,0,items);
+        return size();
+    }
+    /*Update methods*/
+    @Override
+    public T replace(int pos,T item){
+        T ejected=at(pos);
+        
+        return ejected;
+    }
+    @Override
+    public ArrayList<Entry<Integer,T>> replace(ListInterface<Entry<Integer, T>> posItemPair){
+        ArrayList<Entry<Integer,T>> = new ArrayList<>();
+        
+    }
+    
+    
+    private ArrayList<T> wrap(T item){
+        T[] items=(T[]) new Object[1];items[0]=item;
+        return new ArrayList<>(items);
+    }
+    
+    /*Special mutators*/
+    
+    /*Create methods*/
+    
     public boolean delete(int pos){
         return delete(pos,1);
     }
-    public boolean delete(int pos,int count){
+    public boolean delete(int pos,T item){
         splice(pos,count,(T[])new Object[0]);
         return true;
     }
@@ -72,14 +154,14 @@ public class ArrayList<T> implements ListInterface<T>, Serializable {
     public T remove(int givenPosition) {
         T result = null;
 
-        if ((givenPosition >= 1) && (givenPosition <= numberOfEntries)) {
+        if ((givenPosition >= 1) && (givenPosition <= size)) {
             result = array[givenPosition - 1];
 
-            if (givenPosition < numberOfEntries) {
+            if (givenPosition < size) {
                 removeGap(givenPosition);
             }
 
-            numberOfEntries--;
+            size--;
         }
 
         return result;
@@ -87,14 +169,14 @@ public class ArrayList<T> implements ListInterface<T>, Serializable {
 
     @Override
     public void clear() {
-        numberOfEntries = 0;
+        size = 0;
     }
 
     @Override
     public boolean replace(int givenPosition, T newEntry) {
         boolean isSuccessful = true;
 
-        if ((givenPosition >= 1) && (givenPosition <= numberOfEntries)) {
+        if ((givenPosition >= 1) && (givenPosition <= size)) {
             array[givenPosition - 1] = newEntry;
         } else {
             isSuccessful = false;
@@ -107,7 +189,7 @@ public class ArrayList<T> implements ListInterface<T>, Serializable {
     public T getEntry(int givenPosition) {
         T result = null;
 
-        if ((givenPosition >= 1) && (givenPosition <= numberOfEntries)) {
+        if ((givenPosition >= 1) && (givenPosition <= size)) {
             result = array[givenPosition - 1];
         }
 
@@ -117,7 +199,7 @@ public class ArrayList<T> implements ListInterface<T>, Serializable {
     @Override
     public boolean contains(T anEntry) {
         boolean found = false;
-        for (int index = 0; !found && (index < numberOfEntries); index++) {
+        for (int index = 0; !found && (index < size); index++) {
             if (anEntry.equals(array[index])) {
                 found = true;
             }
@@ -126,24 +208,24 @@ public class ArrayList<T> implements ListInterface<T>, Serializable {
     }
 
     @Override
-    public int getNumberOfEntries() {
-        return numberOfEntries;
+    public int getSize() {
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return numberOfEntries < 1;
+        return size < 1;
     }
 
     @Override
     public boolean isFull() {
-        return numberOfEntries == array.length;
+        return size == array.length;
     }
 
     @Override
     public String toString() {
         String outputStr = "";
-        for (int index = 0; index < numberOfEntries; ++index) {
+        for (int index = 0; index < size; ++index) {
             outputStr += array[index] + "\n";
         }
 
@@ -170,7 +252,7 @@ public class ArrayList<T> implements ListInterface<T>, Serializable {
     }
     
     private int getVacantSpace(){
-        return array.length-numberOfEntries;
+        return array.length-size;
     }
     
     private T[] splice(int start,int delCount,T[] items){
@@ -180,11 +262,55 @@ public class ArrayList<T> implements ListInterface<T>, Serializable {
         if (delta>0) expose(start,delta);
         System.arraycopy(items,0,array,start,items.length);
         if (delta<0) cover(start-delta,0-delta);
-        numberOfEntries+=delta;
+        size+=delta;
         return cutout;
     }
     
     public boolean equals(ArrayList<T> list){
-        return numberOfEntries==list.getNumberOfEntries() && getItems(0,numberOfEntries-1)==list.getItems(0,numberOfEntries-1);
+        return size==list.getSize() && getItems(0,size-1)==list.getItems(0,size-1);
     }
+
+    
+
+    @Override
+    public ListInterface<ListInterface<Integer>> getPosOf(ListInterface<T> items) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int append(ListInterface<T> items) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int insert(int pos, ListInterface<T> items) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public T replace(int pos, ListInterface<T> item) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ListInterface<Entry<Integer, T>> replace(Entry<Integer, T>[] posItemPair) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ListInterface<T> delete(ListInterface<Integer> pos) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public T delete(T item) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public T erase(T item) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+   
 }
