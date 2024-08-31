42910b3629b619e7f185e9ea9125889d0af6c392
max011001 <109190703+max011001@users.noreply.github.com>
2024-09-01 01:09:47 +0800

    Add files via upload

---
+++ src/main/java/com/tarumtrsw2407/donationapp202407/utility/MessageUI.java
@@ -0,0 +1,23 @@
+package utility;
+
+/**
+ *
+ * @author Kat Tan
+ */
+public class MessageUI {
+  
+  
+  public static void displayInvalidChoiceMessage() {
+    System.out.println("\nInvalid choice");
+  }
+
+  public static void displayExitMessage() {
+    System.out.println("\nExiting system");
+  }
+  
+  public static void displayInvalidTypeMessage() {
+    System.out.println("\nInvalid Donee type. Please enter 'Individual', 'Organization', or 'Family'.");
+  }
+  
+  
+}
