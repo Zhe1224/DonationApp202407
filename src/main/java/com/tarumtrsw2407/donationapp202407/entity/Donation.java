e8a9daeada7a5592876411a0a4485717997d4d2f
max011001 <109190703+max011001@users.noreply.github.com>
2024-09-01 01:07:39 +0800

    Add files via upload

---
+++ src/main/java/com/tarumtrsw2407/donationapp202407/entity/Donation.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author maxmi
+ */
+public class Donation {
+    private double amount;
+    private String date;
+
+    public Donation(double amount, String date) {
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Amount: RM" + amount + ", Date: " + date;
+    }
+}
